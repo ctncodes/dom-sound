978c284ce972d862eacc025066ec47d9e820124d
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-11-03 23:14:14 -0800

    Detour to Brazil

--- sketchB.js
+++ sketchB.js
@@ -1,9 +1,13 @@
 'use strict';
 
+let osc;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  osc = new p5.Oscillator();
+  osc.freq(240);
+  osc.amp(0);
+  osc.start();
 }
 
 function draw() {
