ef36b3168836fa1aaa443baa15d9c110bba7218b
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-11-06 17:46:21 -0800

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -4,22 +4,22 @@
 
 // Q: Can you add code that asks you to center face in screen?
 
-let capture, tracker;
-let w = windowWidth, h = windowHeight;
+var capture, tracker;
+// var w = windowWidth, h = windowHeight;
 
 function setup() {
     capture = createCapture({
         audio: false,
         video: {
-            width: w,
-            height: h
+            width: windowWidth,
+            height: windowHeight
         }
     }, function() {
         console.log('capture ready.')
     });
     capture.elt.setAttribute('playsinline', '');
-    createCanvas(w, h);
-    capture.size(w, h);
+    createCanvas(windowWidth, windowHeight);
+    capture.size(windowWidth, windowHeight);
     capture.hide();
 
     colorMode(HSB);
@@ -30,31 +30,31 @@ function setup() {
 }
 
 function draw() {
-    image(capture, 0, 0, w, h);
-    let positions = tracker.getCurrentPosition();
+    image(capture, 0, 0, windowWidth, windowHeight);
+    var positions = tracker.getCurrentPosition();
 
     // draws the outine
     // noFill();
     fill(255);
     stroke(255);
     beginShape();
-    for (let i = 0; i < positions.length; i++) {
+    for (var i = 0; i < positions.length; i++) {
         vertex(positions[i][0], positions[i][1]);
     }
     endShape();
 
     noStroke();
-    for (let i = 0; i < positions.length; i++) {
+    for (var i = 0; i < positions.length; i++) {
         fill(map(i, 0, positions.length, 0, 360), 50, 100);
         ellipse(positions[i][0], positions[i][1], 4, 4);
-        text(i, positions[i][0], positions[i][1]);
+        // text(i, positions[i][0], positions[i][1]);
     }
 
   // estimate smiling amount through distance of corners of mouth
     if (positions.length > 0) {
-        let mouthLeft = createVector(positions[44][0], positions[44][1]);
-        let mouthRight = createVector(positions[50][0], positions[50][1]);
-        let smile = mouthLeft.dist(mouthRight);
+        var mouthLeft = createVector(positions[44][0], positions[44][1]);
+        var mouthRight = createVector(positions[50][0], positions[50][1]);
+        var smile = mouthLeft.dist(mouthRight);
 
         // line shows a bar showing smiling amount
         rect(20, 20, smile * 3, 20);
@@ -62,6 +62,6 @@ function draw() {
         // uncomment for a surprise
         noStroke();
         fill(0, 255, 255);
-        ellipse(positions[62][0], positions[62][1], 50, 50);
+        ellipse(positions[62][0], positions[62][1], 100, 100);
     }
 }
