03ce36336df7f33cb6ef181100029d000dc9ebe7
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-11-06 16:57:26 -0800

    The Circus is in town!

--- sketch2.js
+++ sketch2.js
@@ -4,8 +4,8 @@
 
 // Q: Can you add code that asks you to center face in screen?
 
-var capture, tracker;
-var w = 640, h = 480;
+let capture, tracker;
+let w = windowWidth, h = windowHeight;
 
 function setup() {
     capture = createCapture({
@@ -31,20 +31,20 @@ function setup() {
 
 function draw() {
     image(capture, 0, 0, w, h);
-    var positions = tracker.getCurrentPosition();
+    let positions = tracker.getCurrentPosition();
 
     // draws the outine
     // noFill();
     fill(255);
     stroke(255);
     beginShape();
-    for (var i = 0; i < positions.length; i++) {
+    for (let i = 0; i < positions.length; i++) {
         vertex(positions[i][0], positions[i][1]);
     }
     endShape();
 
     noStroke();
-    for (var i = 0; i < positions.length; i++) {
+    for (let i = 0; i < positions.length; i++) {
         fill(map(i, 0, positions.length, 0, 360), 50, 100);
         ellipse(positions[i][0], positions[i][1], 4, 4);
         text(i, positions[i][0], positions[i][1]);
@@ -52,9 +52,9 @@ function draw() {
 
   // estimate smiling amount through distance of corners of mouth
     if (positions.length > 0) {
-        var mouthLeft = createVector(positions[44][0], positions[44][1]);
-        var mouthRight = createVector(positions[50][0], positions[50][1]);
-        var smile = mouthLeft.dist(mouthRight);
+        let mouthLeft = createVector(positions[44][0], positions[44][1]);
+        let mouthRight = createVector(positions[50][0], positions[50][1]);
+        let smile = mouthLeft.dist(mouthRight);
 
         // line shows a bar showing smiling amount
         rect(20, 20, smile * 3, 20);
